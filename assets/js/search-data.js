79489655c0f0c109df024b78a4cb062fe175b7f5
xinwang-cdut <wangxrs@126.com>
2025-06-26 15:02:39 +0000

    Deploying to gh-pages from @ xinwang-cdut/xinwang-cdut.github.io@6acfeee48c964d99f5dd21ca0dd63ca121f95b3b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
