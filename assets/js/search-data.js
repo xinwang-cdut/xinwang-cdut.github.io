b531de71450a6c82902902490891ebc790bafddc
xinwang-cdut <wangxrs@126.com>
2025-06-26 12:47:46 +0000

    Deploying to gh-pages from @ xinwang-cdut/xinwang-cdut.github.io@297b3b6bb37c7b0fda4e6adbbfec917493a61957 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,33 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-news",
-          title: "news",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/news/";
-          },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "publications",
           description: "I have published over 50 academic papers, among which 13 are as the first or corresponding author (Google Scholar H-index = 21). Following are the publications by categories in reversed chronological order.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
           },
         },{id: "nav-cv",
           title: "cv",
